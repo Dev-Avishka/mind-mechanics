4ebd27329c78294bf04f29af6eb05a9cf738ca46
dabdissanayake <dabdissanayake@gmail.com>
2024-12-18 03:19:55 +0000

    lol

--- pages/api/addScore.ts
+++ pages/api/addScore.ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
 import path from 'path';
+import { projectEntrypointsSubscribe } from 'next/dist/build/swc/generated-native';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
