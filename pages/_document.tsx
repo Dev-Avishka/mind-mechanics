fbe1a94409db1872b63cfa4f1319b2b1819c9979
dabdissanayake <dabdissanayake@gmail.com>
2024-12-18 03:33:52 +0000

    ads for my ass

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1934250322552865"></script>
       <body>
         <Main />
         <NextScript />
